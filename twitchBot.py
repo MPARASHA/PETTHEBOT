d8fd6b9499086e70be4cdb53dfe832fc6147b9a3
mparasha-nutrien <110558147+mparasha-nutrien@users.noreply.github.com>
2022-12-26 15:53:43 -0700

    test 4

--- twitchBot.py
+++ twitchBot.py
@@ -121,7 +121,7 @@ class Bot(SingleServerIRCBot):
 		else:
 			schedule = Scheduler(tzinfo=TZ_UTC)
 
-			trigger= dt.time(hour=22, minute=50, tzinfo=TZ_UTC)
+			trigger= dt.time(hour=22, minute=58, tzinfo=TZ_UTC)
 
 			schedule.daily(trigger, self.job)
 
@@ -148,8 +148,7 @@ class Bot(SingleServerIRCBot):
 
 	def job(self):
 		for i in range(7):
-			self.send_message("TriHard")
-			time.sleep(5)
+			self.connection.privmsg(self.CHANNEL, "TriHard")
 		print("executed")
 		
 
